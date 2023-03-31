1af97c87f24a95244e264b9f77ef6a0a12f45669
abu <abubakersuliman@outlook.com>
2023-03-31 21:23:12 +0400

    add model class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+
+class BlogPage (Page):
+    description = models.CharField(max_length=250, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description"),
+    ]
+
+
+class PostPage (Page):
+    header_image = models.ForeignKey("wagtailimages.Image", on_delete=models.SET_NULL, null=True, blank=True,
+                                     related_name='+')
+    content_panels = Page.content_panels + [
+        FieldPanel("header_image"),
+    ]
